a08ce06c0d45ac91607a53c731a4f96f51f76076
chris-zh <chris.zh@outlook.com>
2015-07-18 17:31:08 +0800

    Modify comments

--- quict_sort.py
+++ quict_sort.py
@@ -10,14 +10,14 @@ def quick_sort(a, p, r):
     return a
 
 def partition(a, p, r):
-    i = p - 1#i 在数组之外
+    i = p - 1#i 在数组之外(指向第-1个元素，表示small zone为空)
     key = a[r]#以第r个元素作为key
     for j in range(p, r):#遍历整个数组（除最后一个元素，即最后一次循环为j = r-1）
         if a[j] < key:
-            i = i + 1#i向右移动一个单元
+            i = i + 1#i向右移动一个单元(表示small zone的长度增长1)
             a[i], a[j] = a[j], a[i]#i,j两个元素交换位置，将a[j]放如small zone里
-        #否则，i指针位置不变，j增加。表示本轮循环指向的元素a[j]放入big zone里
-    a[i+1], a[r] = a[r], a[i+1]#最后，将key跟i指向的位置交换（将中轴放在small zone和big zone之间）
+        #否则，i指针位置不变，j增加(表示big zone的长度增长1)。表示本轮循环指向的元素a[j]放入big zone里
+    a[i+1], a[r] = a[r], a[i+1]#最后，将key跟i+1指向的位置交换（将中轴放在small zone和big zone之间）
     #print(a)
-    return i + 1#返回中轴的索引
+    return i + 1#返回中轴的索引（即返回q）
 
